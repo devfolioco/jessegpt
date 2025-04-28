cd572da109b65d82e1bac4cd799d1bfe67ca766e
Pratham Vaidya <iamprathamvaidya@gmail.com>
2025-04-28 16:37:10 +0530

    Replaced GIFs with PNGs

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <main className="min-h-screen flex items-center justify-center bg-[#638596] relative absolute inset-0 h-full w-full bg-[radial-gradient(rgba(229,231,235,0.3)_1px,transparent_2px)] [background-size:36px_36px]">
       <div className="z-10 flex flex-col items-center text-center gap-8">
         <Image
-          src="/mellow-jesse.png"
+          src="/mellow-jesse.gif"
           alt="JesseXBT Avatar"
           width={180}
           height={180}
