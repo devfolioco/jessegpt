f8e58892acf99c5b353ae3f17786227645a4f0eb
Ashwin Kumar Uppala <53075480+ashwinexe@users.noreply.github.com>
2025-05-28 16:25:17 +0530

    update Learn more link to blog

--- client/constants/index.ts
+++ client/constants/index.ts
@@ -1,2 +1,3 @@
 export const BASE_BATCH_APPLY_URL = 'https://nsb.dev/base-batches-2025';
 export const BASE_BATCH_WEBSITE_URL = 'https://www.basebatches.xyz/';
+export const JESSEGPT_BLOG_URL = 'https://devfolio.co/blog/jessegpt/';
